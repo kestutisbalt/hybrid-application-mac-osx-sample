4af323df5d4197eb533a3e3851c4c22c6e0b9b11
Kestutis Baltronas <kestutisbalt@gmail.com>
2015-04-11 22:20:40 +0300

    Added web view to application.

--- hybrid-application-mac-osx-sample/AppDelegate.swift
+++ hybrid-application-mac-osx-sample/AppDelegate.swift
@@ -1,14 +1,16 @@
 import Cocoa
+import WebKit
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
+	@IBOutlet weak var window: NSWindow!
+	@IBOutlet weak var webView: WebView!
 
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-    }
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+	}
 
-    func applicationWillTerminate(aNotification: NSNotification) {
-    }
+	func applicationWillTerminate(aNotification: NSNotification) {
+	}
 
 }
