37fbd1dea0c3dcff6f6767072cda6127f3b93832
Kestutis Baltronas <kestutisbalt@gmail.com>
2015-04-11 21:54:55 +0300

    Created empty xcode project.

---
+++ hybrid-application-mac-osx-sample/AppDelegate.swift
@@ -0,0 +1,14 @@
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+    }
+
+}
---
+++ hybrid-application-mac-osx-sampleTests/hybrid_application_mac_osx_sampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  hybrid_application_mac_osx_sampleTests.swift
+//  hybrid-application-mac-osx-sampleTests
+//
+//  Created by kestutis on 11/04/15.
+//  Copyright (c) 2015 kestutis. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class hybrid_application_mac_osx_sampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
