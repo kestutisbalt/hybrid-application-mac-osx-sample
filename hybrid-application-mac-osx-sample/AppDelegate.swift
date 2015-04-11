2f691704f7433e4d26d323e2edd66b1adc0c7b4c
Kestutis Baltronas <kestutisbalt@gmail.com>
2015-04-11 22:34:34 +0300

    Updated application window to update web view size on resize.

--- hybrid-application-mac-osx-sample/AppDelegate.swift
+++ hybrid-application-mac-osx-sample/AppDelegate.swift
@@ -7,7 +7,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	@IBOutlet weak var window: NSWindow!
 	@IBOutlet weak var webView: WebView!
 
+	private func setupUI() {
+		self.window.contentView = self.webView
+	}
+
 	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		self.setupUI()
 	}
 
 	func applicationWillTerminate(aNotification: NSNotification) {
